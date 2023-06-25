75a09f507ec09507363f3adc8361714141de2dfe
Soumita Das <soumitad991@gmail.com>
2023-06-25 10:27:15 +0530

    Scanner class int input

---
+++ Usingsc.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Usingsc{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the 1st number:");
+        int a = sc.nextInt();
+        System.out.print("Enter the 2nd number:");
+        int b = sc.nextInt();
+        System.out.print("Sum is :");
+        int c=a+b;
+        System.out.print(c);
+    }
+}
