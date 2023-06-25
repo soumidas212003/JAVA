92c1fe6a902a182fdb865abf5acfc1371064b5c2
Soumita Das <soumitad991@gmail.com>
2023-06-25 19:26:34 +0530

    Scanner class using float input

---
+++ Usingsc_float.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Usingsc_float{ 
+     public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter number:");
+        float a = sc.nextFloat();
+        System.out.print(a);
+        //System.out.print("Enter number:");
+        //double b = sc.nextDouble();
+        //System.out.print(b);
+    }
+}
