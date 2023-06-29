e78e6380bd71f63ef369e1a501c5fd238dc12f47
Soumita Das <soumitad991@gmail.com>
2023-06-29 22:07:39 +0530

    Inverted Half Pyramid with Number Pattern using java

---
+++ InvertedHalfPyramidNum_Pattern.java
@@ -0,0 +1,22 @@
+/*
+ 12345
+ 1234
+ 123
+ 12
+ 1
+ */
+
+import java.util.*;
+public class InvertedHalfPyramidNum_Pattern {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n =sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    } 
+}
