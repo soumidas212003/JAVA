776eeea74d52f54d86dfef51fb81f7c613b84160
Soumita Das <soumitad991@gmail.com>
2023-06-30 23:43:01 +0530

    Half Pyramid (Rotated by 180 deg) Pattern using java

---
+++ HalfPyramid_Pattern180.java
@@ -0,0 +1,24 @@
+/*
+ *****
+  ****
+   ***
+    **
+     * 
+ */
+import java.util.*;
+public class HalfPyramid_Pattern180 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter n value: ");
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+   } 
+}
