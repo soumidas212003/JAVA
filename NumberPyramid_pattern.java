be5f3c5eb29fbefd40ec115abb4f06103656d2f1
Soumita Das <soumitad991@gmail.com>
2023-07-06 00:55:18 +0530

    Number Pyramid pattern using java

---
+++ NumberPyramid_pattern.java
@@ -0,0 +1,24 @@
+/*Number Pyramid 
+    1
+   2 2
+  3 3 3 
+ 4 4 4 4
+5 5 5 5 5
+ */
+import java.util.*;
+public class NumberPyramid_pattern {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter n value: ");
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");   
+            }
+            System.out.println();
+        }
+    }
+}
