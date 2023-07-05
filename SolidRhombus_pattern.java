31d6d182f8b5b0a0013fee8d5d39f909f5c22e18
Soumita Das <soumitad991@gmail.com>
2023-07-06 00:31:50 +0530

    Solid Rhombus pattern using java

---
+++ SolidRhombus_pattern.java
@@ -0,0 +1,26 @@
+/*Solid Rhombus Pattern
+
+         * * * * * 
+       * * * * * 
+     * * * * * 
+   * * * * * 
+ * * * * * 
+ 
+ */
+import java.util.*;
+public class SolidRhombus_pattern {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n Value: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
