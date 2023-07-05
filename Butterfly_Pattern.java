fe2ab89ba3cf1fcd77c8131ce8090c014c94f217
Soumita Das <soumitad991@gmail.com>
2023-07-05 22:10:01 +0530

    Butterfly pattern using java

---
+++ Butterfly_Pattern.java
@@ -0,0 +1,46 @@
+/* Butterfly Pattern
+
+ *             *
+ * *         * *
+ * * *     * * *
+ * * * * * * * *
+ * * * * * * * * 
+ * * *     * * *
+ * *         * *
+ *             *
+ 
+ */
+import java.util.*;
+public class Butterfly_Pattern {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n value : ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int spaces=2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+             for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int spaces=2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();   
+        }
+    } 
+}
