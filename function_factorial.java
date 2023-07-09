7a389ce75daa4ca6e9245ce717f628fbeb38b7a4
Soumita Das <soumitad991@gmail.com>
2023-07-09 22:17:06 +0530

    Factorial calculate using java

---
+++ function_factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class function_factorial {
+    public static int factorial(int num){
+        int count=1;
+        for(int i=1;i<=num;i++){
+            count*=i;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num=sc.nextInt();
+        System.out.print("Factorial is : "+factorial(num));
+    }
+}
