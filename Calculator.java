1b89140d551c8477def09d1b7302dc635fdf22db
Soumita Das <soumitad991@gmail.com>
2023-07-18 22:20:15 +0530

    Calculator using java

--- Calculator.java
+++ Calculator.java
@@ -9,9 +9,9 @@ import java.util.*;
 public class Calculator {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 1st number:");
+        System.out.print("Enter 1st number:");
         int a =sc.nextInt();
-        System.out.println("Enter 2nd number:");
+        System.out.print("Enter 2nd number:");
         int b =sc.nextInt();
         boolean n = true;
         while(n){
@@ -46,5 +46,6 @@ public class Calculator {
                 break;
             }
         }
+        sc.close();
     }
 }
